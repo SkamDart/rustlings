3a5e550069966e05633ec288fb4fbfb5ddb9c7ac
Cameron Dart <cdart@anduril.com>
2019-12-28 10:46:26 -0800

    errors solutions

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -12,8 +12,7 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
+    let cost = total_cost(pretend_user_input).unwrap();
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
@@ -25,7 +24,9 @@ fn main() {
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
-
-    Ok(qty * cost_per_item + processing_fee)
+    let qty = item_quantity.parse::<i32>();
+    match qty {
+        Ok(val) => Ok(val * cost_per_item + processing_fee),
+        Err(msg) => Err(msg)
+    }
 }
